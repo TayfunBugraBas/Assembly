c6787dc6d0c2318ae30a058056e0281b30e6433b
TayfunBugraBas <73062585+TayfunBugraBas@users.noreply.github.com>
2022-11-27 13:04:32 +0300

    Update proje.asm

--- proje.asm
+++ proje.asm
@@ -6,21 +6,24 @@
 main:
 la $t0, arr1           #load adress
 la $t1, buffer 
+la $s1, arr3
+jal procedure
+la $t0, arr4        
+jal procedure
+la $t0, arr3        
 jal procedure
 la $t0, arr2        
 jal procedure
  # prosedür çağrısı
 
-
 j bitti
 
 procedure:
 
+beq $t0, $s1, bitti 
+
 lb  $t2, 0($t0)   #byte olarak al
-beq $t2, '*', bitti   
-
 sb   $t2, 0($t1)   #byte olarak kaydet
-
 
 addi $t0, $t0, 1   #üzerine 1 ekle
 addi $t1, $t1, 1
@@ -28,6 +31,7 @@ addi $t1, $t1, 1
 bne  $t2, $zero, procedure
 
 jr $ra
+
 
 
 bitti:
@@ -40,7 +44,8 @@ syscall
 
 
 .data
-arr1: .asciiz "this string function cuts the string where * symbol seen" 
-arr2: .asciiz "tay*fun"
-
+arr1: .asciiz "hello" 
+arr2: .asciiz "tayfun"
+arr3: .asciiz "*"
+arr4: .asciiz "Mr."
 buffer: .space 256
